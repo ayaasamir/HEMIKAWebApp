af89a58e31815611d69ffae6dc5416bf90640a47
hebafoda39 <43217778+hebafoda39@users.noreply.github.com>
2019-06-24 20:31:53 +0200

    create user.ts

--- Development/HEMIKA/HEMIKA-Webapp/src/main/frontend/src/app/modules/creating-users/components/create-user/create-user.component.ts
+++ Development/HEMIKA/HEMIKA-Webapp/src/main/frontend/src/app/modules/creating-users/components/create-user/create-user.component.ts
@@ -1,3 +1,96 @@
+// import {Component, OnInit} from "@angular/core";
+// import {FormBuilder, FormGroup} from "@angular/forms";
+// import {CreateUserService} from "../../shared/createUserService";
+// import {UserDtoData} from "../../shared/data/user-dto.data";
+// import {UserTypeVtoData} from "../../shared/data/user-type-vto.data";
+// import {LoadService} from "../../../../layout/shared/loadService";
+// import {TransService} from "../../../../layout/shared/transService";
+// import {Router} from "@angular/router";
+//
+// @Component({
+//   selector: 'create-user',
+//   templateUrl: './create-user.component.html',
+//   styleUrls: ['./create-user.component.scss'],
+//   providers : [FormBuilder,CreateUserService,LoadService,TransService,Router]
+// })
+// export class CreateUserComponent implements OnInit{
+//
+//   constructor(private formBuilder: FormBuilder,private transSer:TransService,
+//               private userService: CreateUserService,private router:Router,
+//               private loadSer: LoadService) {}
+//
+//   // constructor(
+//   //   private transfereService:TransfereService,
+//   //   private router:Router) {}
+//   //
+//   // somefunction(data){
+//   //   this.transfereService.setData(data);
+//   //   this.router.navigateByUrl('/reciever');//as per router
+//   // }
+//
+//   userType: UserTypeVtoData [] = [];
+//
+//   ngOnInit() {
+//     this.loadSer.findUserType().subscribe(
+//       res => {
+//         this.userType = res;
+//         console.log(this.userType);
+//       }
+//     );
+//   }
+//
+//   formData: FormGroup = this.formBuilder.group({
+//     nationalID: null,
+//     type: null,
+//     firstName: null,
+//     lastName: null,
+//     address: null,
+//     mobileNumber: null,
+//     userName: null,
+//     birthDate: null,
+//     password: null,
+//     confirmPassword: null,
+//     email: null
+//
+//   });
+//   data =this.formData.get('nationalID').value;
+//
+//   omeFunction(data){
+//     this.transSer.setData(data);
+//     this.router.navigateByUrl('/user/doctor');//as per router
+//   }
+//
+//   submit(): void {
+//     let userData: UserDtoData = new UserDtoData();
+//     userData.nationalID = this.formData.get('nationalID').value;
+//     userData.type = this.formData.get('type').value;
+//     userData.firstName = this.formData.get('firstName').value;
+//     userData.lastName = this.formData.get('lastName').value;
+//     userData.address = this.formData.get('address').value;
+//     userData.mobileNumber = this.formData.get('mobileNumber').value;
+//     userData.userName = this.formData.get('userName').value;
+//     userData.birthDate = this.formData.get('birthDate').value;
+//     userData.password = this.formData.get('password').value;
+//     userData.confirmPassword = this.formData.get('confirmPassword').value;
+//     userData.email = this.formData.get('email').value;
+//
+//     console.log(userData);
+//     this.userService.submit(userData).subscribe(
+//       res => {
+//         console.log('request is success');
+//       },
+//       err => {
+//         console.log(err);
+//       }
+//     );
+//   }
+//
+//
+//
+// }
+//
+//
+///////////////////////////////////////////////////////
 import {Component} from "@angular/core";
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {CreateUserService} from "../../shared/createUserService";
@@ -13,12 +106,14 @@ import {LoadService} from "../../../../layout/shared/loadService";
 })
 export class CreateUserComponent {
 
-  constructor(private formBuilder : FormBuilder ,
-              private userService :CreateUserService ,
-              private loadSer:LoadService) {
-
+  constructor(private formBuilder: FormBuilder,
+              private userService: CreateUserService,
+              private loadSer: LoadService) {
   }
+
+
   userType: UserTypeVtoData [] = [];
+
   ngOnInit() {
     this.loadSer.findUserType().subscribe(
       res => {
@@ -27,39 +122,51 @@ export class CreateUserComponent {
       }
     );
   }
-  formData : FormGroup = this.formBuilder.group({
-    nationalID:null,
-    type:null,
-    firstName :null,
-    lastName :null,
-    address:null,
-    mobileNumber:null,
-    userName :null,
-    birthDate:null,
-    password :null,
-    confirmPassword :null,
-    email :null
 
-});
-  submit():void{
-    let userData :UserDtoData = new UserDtoData();
-    userData.nationalID=this.formData.get('nationalID').value;
-    userData.type=this.formData.get('type').value;
-    userData.firstName=this.formData.get('firstName').value;
-    userData.lastName=this.formData.get('lastName').value;
-    userData.address=this.formData.get('address').value;
-    userData.mobileNumber=this.formData.get('mobileNumber').value;
-    userData.userName=this.formData.get('userName').value;
-    userData.birthDate=this.formData.get('birthDate').value;
-    userData.password=this.formData.get('password').value;
-    userData.confirmPassword=this.formData.get('confirmPassword').value;
-    userData.email=this.formData.get('email').value;
+  formData: FormGroup = this.formBuilder.group({
+    nationalID: null,
+    type: null,
+    firstName: null,
+    lastName: null,
+    address: null,
+    mobileNumber: null,
+    userName: null,
+    birthDate: null,
+    password: null,
+    confirmPassword: null,
+    email: null
 
+  });
+
+  submit(): void {
+    let userData: UserDtoData = new UserDtoData();
+    userData.nationalID = this.formData.get('nationalID').value;
+    userData.type = this.formData.get('type').value;
+    userData.firstName = this.formData.get('firstName').value;
+    userData.lastName = this.formData.get('lastName').value;
+    userData.address = this.formData.get('address').value;
+    userData.mobileNumber = this.formData.get('mobileNumber').value;
+    userData.userName = this.formData.get('userName').value;
+    userData.birthDate = this.formData.get('birthDate').value;
+    userData.password = this.formData.get('password').value;
+    userData.confirmPassword = this.formData.get('confirmPassword').value;
+    userData.email = this.formData.get('email').value;
+    localStorage.setItem('nationalID', userData.nationalID);
     console.log(userData);
     this.userService.submit(userData).subscribe(
-      res=>{console.log('request is success');},
-      err=>{console.log(err);}
+      res => {
+        console.log('request is success');
+
+      },
+      err => {
+        console.log(err);
+      }
     );
   }
 
+
+
 }
+
+
+
