42c59557e15938d2e1797390b665dc612f3a32dc
enasmahmoud96 <43348885+enasmahmoud96@users.noreply.github.com>
2019-06-30 11:55:00 +0200

    patientlist & add patient to visitor

--- Development/HEMIKA/HEMIKA-Webapp/src/main/frontend/src/app/modules/doctor-profile/components/patient-list/patient-list.component.ts
+++ Development/HEMIKA/HEMIKA-Webapp/src/main/frontend/src/app/modules/doctor-profile/components/patient-list/patient-list.component.ts
@@ -1,8 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-// import {PatientListDto} from "./shared/data/patient-list.dto.data";
-// import {PatientListService} from "./shared/patient-list.service";
-// import {patientListVTO} from "./shared/data/patient-list.vto.data";
 
 import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup} from "@angular/forms";
@@ -51,29 +46,8 @@ export class PatientListComponent implements OnInit {
   search(){
     let data: PatientListDto = new PatientListDto();
     data.patientName = this.formData.get('patientName').value;
-    data.patientStatus = this.formData.get('patientStatus').value;
-    data.startDate = this.formData.get('startDate').value;
-    data.endDate = this.formData.get('endDate').value;
     console.log(data);
-    // this.patientListService.search(data).subscribe(
-    //   res=> {console.log("insert data succeeded");},
-    //   err=>{console.log(err);}
-    // );
-
     this.solutionOfSearch(data);
 
-
-
-
-
-    // this.patientListService.findPatieantData().subscribe(
-    //   res=>
-    //   {console.log(res);},
-    //   err=>{console.log(err);}
-    //
-    //
-    //
-    //
-    // )
   }
 }
