a51c4c5910a3ac87a27eac5cdb254ba72701b568
hebafoda39 <43217778+hebafoda39@users.noreply.github.com>
2019-07-02 17:41:00 +0200

    doctors

---
+++ Development/HEMIKA/HEMIKA-Webapp/src/main/frontend/src/app/modules/home/components/doctors/doctors.component.ts
@@ -0,0 +1,13 @@
+
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'doctors',
+  templateUrl: './doctors.component.html',
+  styleUrls: ['doctors.component.css']
+})
+export class DoctorsComponent implements OnInit{
+  constructor(){}
+  ngOnInit(){}
+
+}
